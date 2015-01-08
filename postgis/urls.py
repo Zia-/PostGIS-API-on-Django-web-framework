3c17d0653a8c1450a7821c994a821d20e8379d0f
Zia- <mohammed.zia33@gmail.com>
2015-01-08 21:52:14 +0200

    modified more

--- postgis/urls.py
+++ postgis/urls.py
@@ -10,8 +10,12 @@ urlpatterns = patterns('',
     url(r'^$', views.index_view, name='index_name'),
     # This is just an alert message
     url(r'^alert/', views.alert_view, name='alert_name'),
-    # This is default Db sample output
+    # This is "default" Db all table_schemas and table_name output
     url(r'^defaultdb/', views.defaultdb_view, name='defaultdb_name'),
-    # This is dbconn1 Db sample output
+    # This is "dbconn1" Db all table_schemas and table_name output
     url(r'^dbconn1/', views.dbconn1_view, name='dbconn1_name'),
+    # This is "dbconn1" Db = a particular table and all containing data
+    url(r'^(?P<tbl_name>\w+)/dbconn1/$', views.dbconn1_tbl_view, name='dbconn1_tbl_name'),
+    # This is "dbconn1" Db = a particular table and particular gid
+    url(r'^(?P<tbl_name>\w+)/(?P<gid>\d+)/dbconn1/$', views.dbconn1_tbl_gid_view, name='dbconn1_tbl_gid_name'),
 )
